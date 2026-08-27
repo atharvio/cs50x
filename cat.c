c27bbc9b1d22fd9c06c6e30af8856a6e4a84a81f
atharvio <atharv07upadhyay@gmail.com>
2026-08-27 17:25:23 +0000

    Daily CS50 practice update

---
+++ cat.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n = get_int("What's n? ");
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("meow\n");
+        
+    }
+}
