e7a93080c9eecb62dba30e7ac688a7eccc1e7908
atharvio <atharv07upadhyay@gmail.com>
2026-09-03 18:40:49 +0000

    Daily CS50 practice update

---
+++ uppercase.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Before: ");
+    printf("After:  ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+   {
+      // If s[i] is lowercase
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+    printf("%c", s[i] - 32);
+}
+
+// Else if not lowercase
+else
+{
+    printf("%c", s[i]);
+}
+
+   }
+printf("\n");
+
+}
