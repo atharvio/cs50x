88d1149a595f6c8cc7e0698fa122aa1a971765b2
atharvio <atharv07upadhyay@gmail.com>
2026-09-13 07:33:32 +0000

    Daily CS50 practice update

--- search.c
+++ search.c
@@ -1,19 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
-
-int main (void)
+#include <string.h>
+int main(void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    string strings[] = {"battleship", "boot", "canon", "iron", "thimble", "top hat",};
 
-    int n = get_int("Number: ");
-    for (int i = 0; i < 7; i++)
+    string s = get_string("String: ");
+    for (int i = 0; i < 6; i++)
     {
-        if (numbers[i] == n)
+        if (strcmp(strings[i], s) == 0)
         {
             printf("Found\n");
             return 0;
         }
     }
-    printf("Not found\n");
-    return 1;
+
+printf("Not found\n");
+return 1;
 }
